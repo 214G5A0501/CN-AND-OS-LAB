49f71203347a6d4d33539e3591940954c2a05283
214G5A0501 <111738579+214G5A0501@users.noreply.github.com>
2022-09-06 12:45:02 +0530

    Akshaya Created SJF.c

---
+++ Exp-1/SJF.C
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: G.Akshaya
+   Roll No: 214G5A0501
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 06-09-2022
+   Date of Execution: 06-09-2022
+*/
