d71ee39bf7944db3ab19ed033a1d11a1e57f9f27
214G5A0501 <111738579+214G5A0501@users.noreply.github.com>
2022-10-18 11:59:22 +0530

    Update Dining.c

--- Dining.c
+++ Dining.c
@@ -1,5 +1,5 @@
 Name Of the Candidate: G.Akshaya
    Roll No: 214G5A0501
-   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Title of the Experiment: A program to stimulate Dining-philosophers problem.
    Date of Creation: 18-10-2022
    Date of Execution: 18-10-2022
